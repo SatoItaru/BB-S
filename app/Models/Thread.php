747e59fa8740421e1f47ae6cee6e5cfa946358ba
SatoItaru <itaru1822@gmail.com>
2022-01-20 18:44:34 +0900

    DB-create-store

---
+++ app/Models/Thread.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Thread extends Model
+{
+    protected $table = 'threads';
+
+    protected $fillable = [
+        'name', 'user_id', 'id_user_checked', 'latest_comment_time'
+    ];
+}
